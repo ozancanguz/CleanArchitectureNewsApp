afa520d06d5fed0e3b5d51de02a64503b8c096c5
Ozan <ozancangu24@gmail.com>
2022-11-08 19:53:44 +0300

    -article dao coded for room

---
+++ app/src/main/java/com/ozancanguz/cleanarchitecturenewsapp/db/ArticleDao.kt
@@ -0,0 +1,19 @@
+package com.ozancanguz.cleanarchitecturenewsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.ozancanguz.cleanarchitecturenewsapp.Article
+
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(article: Article):Long
+
+    @Query("Select*from articles")
+    fun getAllArticles():LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+
+}
